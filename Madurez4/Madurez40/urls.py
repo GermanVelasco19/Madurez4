71c6250a8f8e0d360c29c90661043635cb83d8f1
icatalinaajc <112427731+icatalinaajc@users.noreply.github.com>
2022-09-04 22:33:03 +0200

    Avance1Pantalla2

--- Madurez4/Madurez40/urls.py
+++ Madurez4/Madurez40/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('Inicio/', views.inicio, name='Inicio'),
+    path('Segunda/',views.segunda, name='Segunda'),
     path('Tercera/', views.tercera, name='Tercera'),
     path('Quinta/', views.Quinta, name='Quinta'),
 ]
--- Madurez4/Madurez40/views.py
+++ Madurez4/Madurez40/views.py
@@ -4,9 +4,13 @@ from django.http import HttpResponse
 def inicio(request):
     return render(request, 'paginas/Inicio.html')
 
+def segunda(request):
+    return render(request,'paginas/segunda.html')
+
 def tercera(request):
     return render(request, 'paginas/Tercera.html')
     
 def Quinta(request):
     return render(request, 'paginas/Quinta.html')
 # Create your views here.
+
