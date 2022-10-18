6477e27d0314228b2fb4a89b8291df1f045a268f
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2022-10-17 23:52:58 -0500

    pruebas

--- Madurez4/Madurez40/views.py
+++ Madurez4/Madurez40/views.py
@@ -53,7 +53,7 @@ def Catorce(request):
 def verResumen(request,NombreCompleto,nombreEmpresa,Cargo,TipodeIndustria,tamañodeEmpresa,Telefono,Correo,reto1,reto2,Dimension11,Dimension12,Dimension21,Dimension22,Dimension31,Dimension32,Dimension33,Dimension34,Dimension35,Dimension36,Dimension37,Dimension38,NivelIngresos,CostoDirectoComoPorcentaje,CostoDirecto,valorInventario):
     empresa=Empresa.create(NombreCompleto,nombreEmpresa,Cargo,TipodeIndustria,tamañodeEmpresa,Telefono,Correo,reto1,reto2,Dimension11,Dimension12,Dimension21,Dimension22,Dimension31,Dimension32,Dimension33,Dimension34,Dimension35,Dimension36,Dimension37,Dimension38,NivelIngresos,CostoDirectoComoPorcentaje,CostoDirecto,valorInventario)
     
-    return HttpResponse(empresa)
+    return render(request, 'paginas/catorce.html')
 
 class EmpresasView(View):
     def get(self, request):
