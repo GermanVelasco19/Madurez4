94d04bdd026d4b0d2ffb6b2fc3b160d7d2e918f9
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2022-10-19 17:40:14 -0500

    ..

--- Madurez4/Madurez40/urls.py
+++ Madurez4/Madurez40/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Inicio/', views.inicio, name='Inicio'),
+    path('', views.inicio, name='Inicio'),
     path('Segunda/',views.segunda, name='Segunda'),
     path('Tercera/', views.tercera, name='Tercera'),
     path('Cuarta/', views.Cuarta, name='Cuarta'),
