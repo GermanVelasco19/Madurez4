9a928fe60ab2471f386a40b02efedc2a587709ba
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2022-11-10 14:04:14 -0500

    Update settings.py

--- Madurez4/Madurez4/settings.py
+++ Madurez4/Madurez4/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6p9wu#s21yo!(t&&!(+!3_(&pg0gf5%k$daxu&)kt0*zj$ou+p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
