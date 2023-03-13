8a90df9407a170ed07e383c2b8cd5fe59c36cb61
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2023-03-13 14:34:03 -0500

    Update settings.py

--- Madurez4/Madurez4/settings.py
+++ Madurez4/Madurez4/settings.py
@@ -132,4 +132,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'julio06.gv@gmail.com'
-EMAIL_HOST_PASSWORD = 'pmacizvntezygrpi'
+EMAIL_HOST_PASSWORD = 'avpevqlatwsvtezs'
